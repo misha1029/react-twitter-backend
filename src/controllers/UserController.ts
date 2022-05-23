3849e148274e9fd11959f77a79be030c9d50f8cb
Misha <sherstoun001@gmail.com>
2022-05-23 21:51:31 +0300

    created passport LocalStrategy, JWT and registration user, created token

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 import { validationResult } from 'express-validator';
-import { UserModel, UserModelInterface } from '../models/UserModel';
+import { UserModel, UserModelInterface, UserModelDocumentInterface } from '../models/UserModel';
 import { generateMD5 } from '../utils/generateHash';
 import { sendEmail } from '../utils/sendEmail';
-/* import { sendEmail } from '../utils/sendEmail'; */
+import { isValidObjectId } from '../utils/isValidObjectId';
 
 class UserController {
   async index(_: any, res: express.Response): Promise<void> {
@@ -13,6 +15,34 @@ class UserController {
       res.json({
         status: 'success',
         data: users,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
+  }
+
+  async show(req: any, res: express.Response): Promise<void> {
+    try {
+      const userId = req.params.id;
+
+      if (!isValidObjectId(userId)) {
+        res.status(400).send();
+        return;
+      }
+
+      const user = await UserModel.findById(userId).exec();
+
+      if (!user) {
+        res.status(404).send();
+        return;
+      }
+
+      res.json({
+        status: 'success',
+        data: user,
       });
     } catch (error) {
       res.status(500).json({
@@ -34,7 +64,7 @@ class UserController {
         email: req.body.email,
         username: req.body.username,
         fullname: req.body.fullname,
-        password: req.body.password,
+        password: generateMD5(req.body.password + process.env.SECRET_KEY),
         confirmHash: generateMD5(process.env.SECRET_KEY || Math.random().toString()),
       };
 
@@ -44,10 +74,10 @@ class UserController {
         {
           emailFrom: 'admin@twitter.com',
           emailTo: data.email,
-          subject: 'Подтверждение почты Twitter Clone',
+          subject: 'Подтверждение почты Twitter Clone Tutorial',
           html: `Для того, чтобы подтвердить почту, перейдите <a href="http://localhost:${
             process.env.PORT || 8888
-          }/users/verify?hash=${data.confirmHash}">по этой ссылке</a>`,
+          }/auth/verify?hash=${data.confirmHash}">по этой ссылке</a>`,
         },
         (err: Error | null) => {
           if (err) {
@@ -99,7 +129,45 @@ class UserController {
       });
     }
   }
+
+  async afterLogin(req: express.Request, res: express.Response): Promise<void> {
+    try {
+      const user = req.user ? (req.user as UserModelDocumentInterface).toJSON() : undefined;
+      console.log(user);
+      res.json({
+        status: 'success',
+        data: {
+          ...user,
+          token: jwt.sign({ data: req.user }, process.env.SECRET_KEY || '123', {
+            expiresIn: '30 days',
+
+          }),
+          
+        },
+        
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
+  }
+
+  async getUserInfo(req: express.Request, res: express.Response): Promise<void> {
+    try {
+      const user = req.user ? (req.user as UserModelDocumentInterface).toJSON() : undefined;
+      res.json({
+        status: 'success',
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
+  }
 }
 
 export const UserCtrl = new UserController();
-
--- src/core/db.ts
+++ src/core/db.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.Promise = Promise;/* mongodb://localhost:27017 */
-
+mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/twitter');
 
 const db = mongoose.connection;
@@ -9,3 +8,4 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 export { db, mongoose };
+
